b62725605a442b77a25663b2faa578b3dc7c3618
Anthony Scalisi <scalisi.a@gmail.com>
2020-10-12 16:28:31 -0700

    add GOBIN and GOPATH exports

--- exports.zsh
+++ exports.zsh
@@ -94,6 +94,9 @@ export BAT_PAGER=
 
 export EXA_COLORS="da=1;34:gm=1;33:ga=1;32:gd=1;31:gv=1;33:gt=1;37:sn=37:sb=37"
 
+export GOPATH=$HOME/go
+export GOBIN=$GOPATH/bin
+
 if [[ -a "$ZSH_CONF/secrets/exports.zsh" ]]; then
   source "$ZSH_CONF/secrets/exports.zsh"
 fi
