f9dc1c255766d1cc1f4aa44908e0005c9996c314
Anthony Scalisi <scalisi.a@gmail.com>
2020-10-17 21:46:54 -0700

    fix tj/git-extras

--- zinit.zsh
+++ zinit.zsh
@@ -50,7 +50,7 @@ zinit light unixorn/fzf-zsh-plugin
 # NOTE: opens in current window instead of new one
 # zinit ice wait lucid; zinit light valentinocossar/sublime
 
-zinit ice as"program" pick"$ZPFX/bin/git-*" src"etc/git-extras-completion.zsh" make"PREFIX=$ZPFX" wait=1 lucid
+zinit ice wait=1 lucid as"program" pick"$ZPFX/bin/git-*" src"etc/git-extras-completion.zsh" make"PREFIX=$ZPFX"
 zinit light tj/git-extras
 
 zinit wait=1 lucid for            \
