31876f33ffd18b03f43d69a2b9b4491418e98748
Anthony Scalisi <scalisi.a@gmail.com>
2022-08-03 15:57:35 -0700

    add rancher

--- exports.zsh
+++ exports.zsh
@@ -22,15 +22,15 @@ export LC_ALL=en_US.UTF-8
 export PATH=/usr/local/opt/coreutils/libexec/gnubin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/local/opt/ruby/bin:/usr/local/lib/ruby/gems/2.6.0/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/opt/local/bin:/usr/local/sbin:/Applications/Google\ Chrome.app/Contents/MacOS:$PATH
 
 if [[ -a "${HOME}/go/bin" ]]; then
-export PATH="${PATH}:${HOME}/go/bin"
+  export PATH="${PATH}:${HOME}/go/bin"
 fi
 
 if [[ -a "${HOME}/.cargo/bin" ]]; then
-export PATH="${PATH}:${HOME}/.cargo/bin"
+  export PATH="${PATH}:${HOME}/.cargo/bin"
 fi
 
 if [[ -a "${HOME}/.krew/bin" ]]; then
-export PATH="${PATH}:${HOME}/.krew/bin"
+  export PATH="${PATH}:${HOME}/.krew/bin"
 fi
 
 # Don’t clear the screen after quitting a manual page.
@@ -115,6 +115,10 @@ export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig
 
 if [[ -a "$ZSH_CONF/secrets/exports.zsh" ]]; then
   source "$ZSH_CONF/secrets/exports.zsh"
+fi
+
+if [[ -a "${HOME}/.rd/bin" ]]; then
+  export PATH="${PATH}:${HOME}/.rd/bin"
 fi
 
 export TF_PLUGIN_CACHE_DIR="$HOME/.terraform.d/plugin-cache"
